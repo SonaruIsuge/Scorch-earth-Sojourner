3b35708105e58cd0ee6e16cdcc1f37d455203044
SonaruIsuge <bread511206@gmail.com>
2022-05-25 22:05:30 +0800

    commit

--- Assets/Scripts/Machine/M_TurnLeft.cs
+++ Assets/Scripts/Machine/M_TurnLeft.cs
@@ -1,20 +1,26 @@
-﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-
-public class MachineXAxisControlLever : MonoBehaviour, IInteractable
+public class M_TurnLeft : MonoBehaviour, IInteractable
 {
     public bool isInteract { get; private set; }
     public bool isSelect { get; private set; }
     
     private SpriteRenderer spriteRenderer;
 
+    [SerializeField]private bool leftEnable;
+    
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        leftEnable = false;
     }
 
     public void OnSelect()
     {
+        isSelect = true;
         var color = spriteRenderer.color;
         color = new Color(color.r, color.g, color.b, 1);
         spriteRenderer.color = color;
@@ -22,13 +28,16 @@ public class MachineXAxisControlLever : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        
+        leftEnable = !leftEnable;
     }
 
     public void OnDeselect()
     {
+        isSelect = false;
         var color = spriteRenderer.color;
-        color = new Color(color.r, color.g, color.b, 0.5f);
+        color = new Color(color.r, color.g, color.b, leftEnable ? 0.8f : 0.4f);
         spriteRenderer.color = color;
     }
+
+    public bool LeftEnable => leftEnable;
 }
--- Assets/Scripts/Machine/M_TurnRight.cs
+++ Assets/Scripts/Machine/M_TurnRight.cs
@@ -1,21 +1,26 @@
-﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-
-public class MachineYAxisControlLever : MonoBehaviour, IInteractable
+public class M_TurnRight : MonoBehaviour, IInteractable
 {
-
     public bool isInteract { get; private set; }
     public bool isSelect { get; private set; }
     
     private SpriteRenderer spriteRenderer;
 
+    [SerializeField]private bool rightEnable;
+    
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        rightEnable = false;
     }
 
     public void OnSelect()
     {
+        isSelect = true;
         var color = spriteRenderer.color;
         color = new Color(color.r, color.g, color.b, 1);
         spriteRenderer.color = color;
@@ -23,14 +28,16 @@ public class MachineYAxisControlLever : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        
+        rightEnable = !rightEnable;
     }
 
     public void OnDeselect()
     {
+        isSelect = false;
         var color = spriteRenderer.color;
-        color = new Color(color.r, color.g, color.b, 0.5f);
+        color = new Color(color.r, color.g, color.b, rightEnable ? 0.8f : 0.4f);
         spriteRenderer.color = color;
     }
 
+    public bool RightEnable => rightEnable;
 }
