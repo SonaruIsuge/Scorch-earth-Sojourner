2322efa2159f6aa03234d7e2034d5a02be181192
SonaruIsuge <bread511206@gmail.com>
2022-05-29 19:43:09 +0800

    Add machine background and camera control

---
+++ Assets/Scripts/Camera/CameraFollowControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowControl : MonoBehaviour
+{
+    [SerializeField]private Transform target;
+    private Camera mainCamera;
+    [SerializeField] private float xMinPos;
+    [SerializeField] private float xMaxPos;
+    [SerializeField] private float yMinPos;
+    [SerializeField] private float yMaxPos;
+
+    private float cameraHalfHeight;
+    private float cameraHalfWidth;
+    private bool hasTarget => target != null;
+
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        cameraHalfHeight = mainCamera.orthographicSize;
+        cameraHalfWidth = cameraHalfHeight * ((float)Screen.width / Screen.height);
+        
+        if (!hasTarget) return;
+        
+        var cameraX = Mathf.Clamp(target.position.x, xMinPos + cameraHalfWidth, xMaxPos - cameraHalfWidth);
+        var cameraY = Mathf.Clamp(target.position.y, yMinPos + cameraHalfHeight, yMaxPos - cameraHalfHeight);
+        transform.position = new Vector3(cameraX, cameraY, transform.position.z);
+    }
+}
