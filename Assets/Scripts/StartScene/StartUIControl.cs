dd31de30ae24e1c9c9bd8198e65f5b8e55ccdfcb
SonaruIsuge <bread511206@gmail.com>
2022-11-29 01:08:00 +0800

    Game Start art change

--- Assets/Scripts/StartScene/StartUIControl.cs
+++ Assets/Scripts/StartScene/StartUIControl.cs
@@ -6,22 +6,31 @@ using UnityEngine.UI;
 public class StartUIControl : MonoBehaviour
 {
     [SerializeField] private Button StartGameBtn;
-
+    [SerializeField] private Button ExitGameBtn;
+    
 
     private void OnEnable()
     {
         StartGameBtn.onClick.AddListener(StartGame);
+        ExitGameBtn.onClick.AddListener(ExitGame);
     }
 
 
     private void OnDisable()
     {
         StartGameBtn.onClick.RemoveAllListeners();
+        ExitGameBtn.onClick.RemoveAllListeners();
     }
 
 
     private void StartGame()
     {
         GameFlowHandler.Instance.LoadScene(SceneIndex.Level, new LevelData());
+    }
+
+
+    private void ExitGame()
+    {
+        Application.Quit();
     }
 }
